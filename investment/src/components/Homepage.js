8bf151dfa3a286dab7c5215f2298a9b266417c0b
Blaque <blaque.develop@gmail.com>
2023-10-06 01:36:27 +0300

    Merged

--- investment/src/components/Homepage.js
+++ investment/src/components/Homepage.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react'
-import '../App.css'
-import { Login } from '../components2/Login'
-import { SignUp } from '../components2/SignUp';
-
+import React, { useState } from 'react';
+import '../App.css';
+import { Login } from './Login';
+import { SignUp } from './SignUp';
 
 function Homepage() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -10,15 +9,28 @@ function Homepage() {
   const toggleForm = (formName) => {
     setCurrentForm(formName);
   }
+
   return (
-    
-       <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <SignUp onFormSwitch={toggleForm} />
-      }
+    <div className="App">
+      <div className="auth-form-container">
+        {currentForm === "login" ? (
+          <div>
+            <Login />
+            <button className="link-btn" onClick={() => toggleForm('register')}>
+              Don't have an account? Register here.
+            </button>
+          </div>
+        ) : (
+          <div>
+            <SignUp />
+            <button className="link-btn" onClick={() => toggleForm('login')}>
+              Already have an account? Login here.
+            </button>
+          </div>
+        )}
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
