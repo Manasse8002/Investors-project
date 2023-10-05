8c55a3f12e8b46d3ebd1ee321bbfd80c18bc6b47
Blaque <blaque.develop@gmail.com>
2023-10-06 02:37:34 +0300

    Changes

--- investment/src/components/Homepage.js
+++ investment/src/components/Homepage.js
@@ -1,27 +1,3 @@
-// import React, { useState } from 'react'
-// import '../App.css'
-// import { Login } from './Login';
-// import { SignUp } from './SignUp';
-
-// function Homepage() {
-//   const [currentForm, setCurrentForm] = useState('login');
-
-//   const toggleForm = (formName) => {
-//     setCurrentForm(formName);
-//   }
-//   return (
-    
-//        <div className="App">
-//       {
-//         currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <SignUp onFormSwitch={toggleForm} />
-//       }
-//     </div>
-    
-//   )
-// }
-
-// export default Homepage
-
 import React, { useState } from 'react';
 import '../App.css';
 import { Login } from './Login';
--- investment/src/index.js
+++ investment/src/index.js
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-// import {createRoot} from 'react-dom/client';
 
-// const rootElement = document.getElementById('root');
-// const root = createRoot(rootElement);
-// root.render(
-  const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
-  // document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
